fd0025e6f2763886483d538a2bf05c456bf549f9
sainarasimhayandamuri <38848935+sainarasimhayandamuri@users.noreply.github.com>
2018-05-25 09:44:45 +0530

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -5,15 +5,12 @@ katamarayudu = media.movie("Katamarayudu",
                            "Story about violence",
                            "https://bit.ly/2LmxFRs", "https://bit.ly/2mES06P")
 agnyathavasi = media.movie("Agnyathavasi", "Story about prince",
-                           "https://bit.ly/2s0Ia47",
-                           "https://www.youtube.com/watch?v=97h9fBWltBM")
+                           "https://bit.ly/2s0Ia47", "https://bit.ly/2D6FEOI")
 gabbarsingh = media.movie("Gabbarsingh", "Story about cop",
-                          "https://bit.ly/2GCGOli",
-                          "https://www.youtube.com/watch?v=FpFoQPj4sgs")
+                          "https://bit.ly/2GCGOli", "https://bit.ly/2GPtWIv")
 rambabu = media.movie("Rambabu", "Story about power of common man",
-                      "https://bit.ly/2IEMAEX",
-                      "https://www.youtube.com/watch?v=TtcEOM7Fmy0")
+                      "https://bit.ly/2IEMAEX", "https://bit.ly/2xbzoGi")
 
 movies = [katamarayudu, agnyathavasi, gabbarsingh, rambabu]
+# used to open movie trailers web page
 fresh_tomatoes.open_movies_page(movies)
-
